4cbb0154b689c123e7f93197acffb92a4c9dd15c
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2022-10-21 09:28:10 -0400

    Create Day 5-Even Numbers In An Array.js

---
+++ Challenges/Day 5-Even Numbers In An Array.js
@@ -0,0 +1,9 @@
+Even numbers In An Array
+
+function evenNumbers(array, number) {
+ 	return array.filter(x => x % 2 === 0).slice(-number)
+}
+
+console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3), [4,6,8])
+console.log(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1), [6])
+console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2), [-8,26])
