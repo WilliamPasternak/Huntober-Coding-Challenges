703ec2bb66316521d453b66ea7a79f1f52ee3817
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2022-10-27 01:40:46 -0400

    Create Day 8-DuplicateEncoder.js

---
+++ Challenges/Day 8-DuplicateEncoder.js
@@ -0,0 +1,29 @@
+function duplicateEncode(word){
+  word = word.toLowerCase()
+  let chars = word.split('')
+  const lookup = {}
+  
+// Count occurances in lookup object
+  for(char of chars){
+	lookup[char] = (lookup[char] || 0 ) + 1
+}
+
+// Update array based on char count
+for(let i = 0; i < chars.length; i++){
+  if(lookup[chars[i]] === 1){
+    chars[i] = '('
+  }
+  else{
+    chars[i] = ')'
+  }
+  
+}
+
+return chars.join('')
+}
+
+// Tests
+console.log(duplicateEncode("din"),"(((")
+console.log(duplicateEncode("recede"),"()()()")
+console.log(duplicateEncode("Success"),")())())")
+console.log(duplicateEncode("(( @"),"))((")
