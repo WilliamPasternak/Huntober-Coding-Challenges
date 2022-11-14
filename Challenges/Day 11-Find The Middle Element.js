96cba465bfc3af628a821f608a63b28c6dc629f7
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2022-11-14 16:09:52 -0500

    Create Day 11-Find The Middle Element.js

---
+++ Challenges/Day 11-Find The Middle Element.js
@@ -0,0 +1,19 @@
+You need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+The input to the function will be an array of three distinct numbers.
+
+Examples:
+gimme([2, 3, 1]) => 0
+2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+gimme([5, 10, 14]) => 1
+10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+function gimme (triplet) {
+  const minIndex = triplet.indexOf(Math.min(...triplet))
+  const maxIndex = triplet.indexOf(Math.max(...triplet))
+  return 3 - minIndex - maxIndex
+}
+
+// Test Cases:
+console.log(gimme([2, 3, 1]), '0')
+console.log(gimme([5, 10, 14]), '1')
