cac197b224b611c4ec74a3033847f36d96cdbd39
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2023-01-28 17:21:01 -0500

    Last Challenge: Shopping Cart

---
+++ Other JS Challenges/Shopping Cart.js
@@ -0,0 +1,15 @@
+import shoppingCart from "./data.js";
+
+/*  
+Use reduce() to total the groceries. 
+Then find a method that will round the total to 2 decimal places.
+
+Example output: 73.44
+*/
+
+function total(arr){
+    return arr.reduce((acc,cur) => acc + cur.price)
+              .toFixed(2)
+}
+
+console.log(total(shoppingCart));
